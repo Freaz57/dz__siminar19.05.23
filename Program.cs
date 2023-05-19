8c9295002c5bb3ffaddb951fc4d2aae38a8a1317
Ilya <ok.brain@yandex.ru>
2023-05-20 00:42:47 +0300

    готовое дз

--- Program.cs
+++ Program.cs
@@ -1,2 +1,104 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+// Console.WriteLine("Введите число");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num>num2) {
+// Console.WriteLine(num);
+// } else {
+//     Console.WriteLine(num2);
+// }
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+// Console.WriteLine("Введите число");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+
+//  int num2 = Convert.ToInt32(Console.ReadLine());
+
+//  int num3 = Convert.ToInt32(Console.ReadLine());
+
+//  if((num>num2) && (num>num3))  {
+// Console.WriteLine(num);
+// } else if (num2 > num3) {
+//     Console.WriteLine(num2);
+// } else {
+//     Console.WriteLine(num3);
+// }
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+
+
+// Console.WriteLine("Введите число");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int sum = num % 2;
+
+// if(sum == 0)  {
+//     Console.WriteLine("true");
+// } 
+//  else {
+//     Console.WriteLine("false");
+// }
+
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+
+//   Console.WriteLine("Введите число"); 
+// int num = Convert.ToInt32 (Console.ReadLine());
+
+
+// int evenNumbers = 1 ;
+// while( evenNumbers <= num)
+// {
+// if(evenNumbers % 2 != 1){
+// Console.Write(evenNumbers+ ";");
+// }
+// evenNumbers++;
+// }
+
+
+            
+
+
+// if((num >1) && (num<10))
+// {
+// int sum = num % 10 ;
+// Console.Write(sum);
+// }
+
+// else {
+//    Console.WriteLine("false"); 
+// }
+
+
+
+
+
+
+
+
